76799761fa7a7dfe2c4d13d45866332316f9a38b
EASYNETY <israeladeyeyee@gmail.com>
2022-02-12 09:43:18 +0100

    New changes made

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,9 +1,7 @@
-import React, {useState} from "react";
-import { Link } from "react-router-dom"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import styled, { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme, GlobalStyles } from "../components/themes";
-
-
 
 function NavBar(props) {
   const [theme, setTheme] = useState("light");
@@ -40,25 +38,31 @@ function NavBar(props) {
             />
           </Link>
 
-          <button
-            className="navbar-toggler d-lg-none"
+          {/* <button
+            className="navbar-toggler"
             type="button"
-            data-toggle="navbarToggler"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
+            <i className="fas fa-bars text-white"></i>
+          </button> */}
+
+          {/* <div className="navbar-inner ml-lg-auto pl-lg-2 pl-xl-6"> */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i className="fas fa-bars text-white"></i>
           </button>
-
-          <div className="navbar-inner ml-lg-auto pl-lg-2 pl-xl-6">
-            <button
-              className="navbar-toggler d-lg-none"
-              type="button"
-              data-toggle="navbarToggler"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <nav>
               <ul className="navbar-nav" id="navbar-nav">
                 <li className="nav-item active">
