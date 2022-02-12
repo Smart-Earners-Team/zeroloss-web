91ec995f70d042ae86e080065f4990f25fc6e3b2
EASYNETY <israeladeyeyee@gmail.com>
2022-02-12 09:30:26 +0100

    New changes made

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -2,8 +2,6 @@ import React, {useState} from "react";
 import { Link } from "react-router-dom"
 import styled, { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme, GlobalStyles } from "../components/themes";
-import ContentLayout from "./content-layout";
-import "../assets/styles/navbar.css"
 
 
 
@@ -12,132 +10,140 @@ function NavBar(props) {
   const themeToggler = () => {
     theme === "light" ? setTheme("dark") : setTheme("light");
   };
- const [dropDown, setDropDown] = useState(false);
-
-
 
   return (
-    <header className="navbar navbar-sticky navbar-expand-lg navbar-dark" style={{backgroundColor: "black"}}>
-    <div className="container ">
-    <nav
-      className="navbar navbar-expand-lg navbar-mainbg"
-      style={{ backgroundColor: "black", position:"fixed", height: "auto" }}
+    <div
+      className="theme"
+      data-spy="scroll"
+      data-target="#navbar-nav"
+      data-animation="true"
+      data-appearance="light"
     >
-      <a className="navbar-brand navbar-logo" href="/">
-        <img
-          className="navbar-brand__regular"
-          src="images/Zeroloss_logo.png"
-          alt="sticky brand-logo"
-        />
-      </a>
-
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
+      <button onClick={() => themeToggler()}>Change Theme</button>
+      <header
+        className="navbar navbar-sticky navbar-expand-lg navbar-dark"
+        style={{ backgroundColor: "black", position: "fixed" }}
       >
-        <i className="fas fa-bars text-white"></i>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav ml-auto">
-                   <li className="nav-item ">
-            <Link
-              className="nav-link"
-              // href="/"
-              style={{ color: "white" }}
-              activeClassName="is-active"
-              to="/"
-              exact
-            >
-              Home
-            </Link>
-          </li>
-          <li className="nav-item ">
-            <Link
-              className="nav-link"
-              // href="/defi"
-              style={{ color: "white" }}
-              activeClassName="is-active"
-              to="/defi"
-              exact
-            >
-              Launch App
-            </Link>
-          </li>
-          <li className="nav-item ">
-            <Link
-              className="nav-link"
-              // href=
-              style={{ color: "white" }}
-              activeClassName="is-active"
-              to="/idolaunchpad"
-              exact
-            >
-              ZerolossPad
-            </Link>
-          </li>
-          <li className="nav-item ">
-            <a
-              className="nav-link"
-              href="https://front.herokuapp.com/rugchecker"
-              style={{ color: "white" }}
-              activeClassName="is-active"
-              exact
-            >
-              Rug Checker
-            </a>
-          </li>
-          <li className="nav-item ">
-            <a
-              className="nav-link"
-              href="https://drive.google.com/file/d/1KvACbgj-2-ZrsM8v_UHj7BuV__OjoewV/view?usp=drivesdk"
-              style={{ color: "white" }}
-              activeClassName="is-active"
-              exact
-            >
-              LightPaper
-            </a>
-          </li>
-          <li className="nav-item ">
-            <Link
-              className="nav-link"
-              // href=
-              style={{ color: "white" }}
-              activeClassName="is-active"
-              to="/faq"
-              exact
-            >
-              FAQ
-            </Link>
-          </li>
-
-          <div className="mr-5 mr-lg-0 ml-lg-2 mt-1">
-          <Link
-            to="/defi"
-            className="btn btn-size--sm btn-border btn-hover--danger color--white mr-5" 
-          >
-            <span className="btn__text font-w--500">Buy</span>
+        <div className="container position-relative">
+          <Link className="navbar-brand" to="/">
+            <img
+              className="navbar-brand__regular"
+              style={{ height: "120px" }}
+              src="images/Zeroloss_logo.png"
+              alt="brand-logo"
+            />
+            <img
+              className="navbar-brand__sticky"
+              src="images/Zeroloss_logo.png"
+              style={{ height: "120px" }}
+              alt="sticky brand-logo"
+            />
           </Link>
-          <Link
-            to="/defi"
-            className="btn btn-size--sm btn-hover--splash mr-5"
-            style={{ backgroundColor: "gold" }}
+
+          <button
+            className="navbar-toggler d-lg-none"
+            type="button"
+            data-toggle="navbarToggler"
+            aria-label="Toggle navigation"
           >
-            <span className="btn__text">DeFi</span>
-          </Link>
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="navbar-inner ml-lg-auto pl-lg-2 pl-xl-6">
+            <button
+              className="navbar-toggler d-lg-none"
+              type="button"
+              data-toggle="navbarToggler"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <nav>
+              <ul className="navbar-nav" id="navbar-nav">
+                <li className="nav-item active">
+                  <Link
+                    className="nav-link"
+                    style={{ color: "white" }}
+                    activeClassName="is-active"
+                    to="/"
+                    exact
+                  >
+                    Home
+                  </Link>
+                </li>
+
+                <li className="nav-item ">
+                  <Link
+                    className="nav-link"
+                    to="/defi"
+                    style={{ color: "white" }}
+                  >
+                    Launch App
+                  </Link>
+                </li>
+
+                <li className="nav-item ">
+                  <Link
+                    className="nav-link"
+                    to="/idolaunchpad"
+                    style={{ color: "white" }}
+                  >
+                    ZerolossPad
+                  </Link>
+                </li>
+
+                <li className="nav-item ">
+                  <a
+                    className="nav-link"
+                    href="https://front.herokuapp.com/rugchecker"
+                    style={{ color: "white" }}
+                  >
+                    Rug Checker
+                  </a>
+                </li>
+
+                <li className="nav-item ">
+                  <a
+                    className="nav-link"
+                    href="https://drive.google.com/file/d/1KvACbgj-2-ZrsM8v_UHj7BuV__OjoewV/view?usp=drivesdk"
+                    style={{ color: "white" }}
+                  >
+                    LightPaper
+                  </a>
+                </li>
+
+                <li className="nav-item ">
+                  <Link
+                    className="nav-link"
+                    to="/faq"
+                    style={{ color: "white" }}
+                  >
+                    FAQ
+                  </Link>
+                </li>
+              </ul>
+            </nav>
           </div>
-        </ul>
-      </div>
-    </nav>
+          <div className="mr-5 mr-lg-0 ml-lg-2">
+            <Link
+              to="/defi"
+              className="btn btn-size--sm btn-border btn-hover--danger color--white"
+            >
+              <span className="btn__text font-w--500">Buy</span>
+            </Link>
+            <Link
+              to="/defi"
+              target="_blank"
+              className="btn btn-size--sm btn-hover--splash"
+              style={{ backgroundColor: "gold" }}
+            >
+              <span className="btn__text">DeFi</span>
+            </Link>
+          </div>
+        </div>
+      </header>
     </div>
-    </header>
-    
-    
   );
 }
 
